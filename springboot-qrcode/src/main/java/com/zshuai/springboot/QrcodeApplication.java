1a3df624a6dbbb7120167f912c812a18859b8ff1
zshuaigit <zshuai1722@163.com>
2020-04-24 16:18:26 +0800

    springboot实现二维码

---
+++ springboot-qrcode/src/main/java/com/zshuai/springboot/QrcodeApplication.java
@@ -0,0 +1,17 @@
+package com.zshuai.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zshuai
+ *
+ * @Date :2020/4/24 2:49 PM
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class QrcodeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QrcodeApplication.class);
+    }
+}
